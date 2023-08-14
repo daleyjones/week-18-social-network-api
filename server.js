a020bc2c20bb5a278f87420af90df64d4a5cd00e
daleyjones <djones_15@hotmail.com>
2023-08-14 12:08:09 +0100

    removed debug

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/week-18-s
   useUnifiedTopology: true
 });
 
-mongoose.set('debug', true);
+
 
 
 app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
