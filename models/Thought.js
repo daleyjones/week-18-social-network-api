b8facb2aef23ff9924c79ae6b19a7d82e9cce794
daleyjones <djones_15@hotmail.com>
2023-08-14 09:29:40 +0100

    added folders

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = require('./Reaction');
+
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+
+
+{
+  thoughtText: {
+    type: String,
+    required: 'You need to leave a thought',
+    minLength: 1,
+    maxLength: 280
+  },
+  createdAt : {
+    type: Date,
+    default: Date.now,
+    get: timestamp => dateFormat(timestamp)
+  },
+  usernames: {
+    type: String,
+    required: true
+},
+reactions: [reactionSchema]
+},
+{
+
+toJSON: {
+  getters: true
+},
+id: false,
+}
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+  return this.reactions.length;
+
+});
+
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
